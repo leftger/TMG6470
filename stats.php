dfa76c26856c0f5ebd76d57178b7f2335e07ffca
Gerzain Rosas <grosas@mit.edu>
2012-01-17 15:59:08 -0500

    added a HUGE XML file showing basic congressman info.  I also made a script where I can display all the info into a PHP file

--- stats.php
+++ stats.php
@@ -2,8 +2,36 @@
 <html>
 	<head>
 		<?php
-			$candidate_id = 400311;
-			$govtrack_photos_url = "http://govtrack.us/data/photos/";
+		$candidate_id = 400311;
+		$govtrack_photos_url = "http://govtrack.us/data/photos/";
+		// start MySQL connection
+		$con = mysql_connect("localhost","root","");
+		if(!$con)
+		{
+			 die('Could not connect: ' . mysql_error());
+		}
+		if (mysql_query("CREATE DATABASE candidates_2012", $con))
+		{
+			echo "Database created";
+		}
+		else {
+			echo "Error creating database: " . mysql_error();
+		}
+			
+		// Create table
+		mysql_select_db("candidates_2012", $con);
+		$sql = "CREATE TABLE stats
+		(
+		FirstName varchar(15),
+		LastName varchar(15),
+		Age int,
+		ServedSince TIME,
+		BillsSponsored int,
+		BillsPassed int
+		)";
+		mysql_query($sql, $con);
+		
+		mysql_close($con);
 		?>
 		<link rel="icon" type="image/ico" href="favicon.ico"> 
 		<link rel="stylesheet" href="stats.css" type="text/css" />
@@ -13,37 +41,22 @@
 		<header></header>
 		<h1>
 			<img src="<?php echo $govtrack_photos_url . $candidate_id; ?>.jpeg" alt = "Ron Paul's face" />
-			<?php 
-			$con = mysql_connect("localhost","root","");
-			if(!$con)
-			{
-				 die('Could not connect: ' . mysql_error());
-			}
-			if (mysql_query("CREATE DATABASE candidates_2012", $con))
-			{
-				echo "Database created";
-			}
-			else {
-				echo "Error creating database: " . mysql_error();
-			}
-			
-			
-			// Create table
-			mysql_select_db("candidates_2012", $con);
-			$sql = "CREATE TABLE stats
-			(
-			FirstName varchar(15),
-			LastName varchar(15),
-			Age int,
-			ServedSince TIME,
-			BillsSponsored int,
-			BillsPassed int
-			)";
-			mysql_query($sql, $con);
-			
-			mysql_close($con);
-			?>
 		</h1>
+		<?php
+		// Load XML file
+		$xml = simplexml_load_file("people.xml");
+		
+		echo $xml->getName() . "<br />";
+		
+		foreach($xml->children() as $person)
+		{
+			foreach($person->attributes() as $a => $b)
+			{
+				echo $a . ": " . $b . " ";
+			}
+			echo "<br />";			
+		}
+		?>
 		<footer></footer>
 	</body>
 </html>
