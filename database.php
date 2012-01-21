c00cf00e94ab8cc7b6a9c15713b4f739185c6bae
Michael Duplessis <mdupless@mit.edu>
2012-01-21 15:26:11 -0500

    Decent layout, logo w/o title decent nav bar

---
+++ database.php
@@ -0,0 +1,9 @@
+<?php
+$host = '18.194.1.84:3306';
+$username = 'root';
+$password = '';
+$db = mysql_connect($host, $username, $password) or die('Could not connect: ' . mysql_error());
+mysql_select_db('candidates_2012');
+mysql_query("CREATE TABLE login", $db);
+
+?>
