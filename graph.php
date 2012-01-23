185d2ef13dc99e7373ba4e2fed0814cff5f6afed
Thane Miller <thanehmiller@gmail.com>
2012-01-22 22:40:46 -0500

    Graphing from the database!

--- graph.php
+++ graph.php
@@ -1,26 +1,32 @@
 <?php
+
+
 include("phpgraphlib.php");
 $graph=new PHPGraphLib(550,350);
-$link = mysql_connect('localhost', 'TMG6470', 'snett')
+$link = mysql_connect('18.194.1.88:3306', 'TMG6470', 'snett')
    or die('Could not connect: ' . mysql_error());
      
-mysql_select_db('gallup_poptime') or die('Could not select database');
+mysql_select_db('candidates_2012') or die('Could not select database');
   
 $dataArray=array();
-  
+$count=0; 
 //get data from database
-$sql="SELECT Gingrich COUNT(*), AS 'count' FROM Date GROUP BY Gingrich";
+$sql="SELECT Gingrich,Date FROM gallup_poptime";
+//$dates="SELECT Date FROM gallup_poptime";
 $result = mysql_query($sql) or die('Query failed: ' . mysql_error());
+//$result2 = mysql_query($dates) or die('Query failed: ' . mysql_error());
 if ($result) {
   while ($row = mysql_fetch_assoc($result)) {
       $salesgroup=$row["Gingrich"];
-      $count=$row["Date"];
-      //add to data areray
-      $dataArray[$salesgroup]=$count;
+      $dates=$row["Date"];
+      //$date=$result2[$count];
+      //add to data array
+      $dataArray[$dates]=$salesgroup;
+	  $count++;
   }
 }
-  
-//configure graph
+
+//print_r($dataArray);
 $graph->addData($dataArray);
 $graph->setTitle("Popularity");
 $graph->setBars(false);
@@ -28,7 +34,7 @@ $graph->setLine(true);
 $graph->setDataPoints(true);
 $graph->setDataPointColor('maroon');
 $graph->setDataValues(true);
-$graph->setDataValueColor('black');
+$graph->setDataValueColor('maroon');
 $graph->setGoalLine(.0025);
 $graph->setGoalLineColor('red');
 $graph->createGraph();
